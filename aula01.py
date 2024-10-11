8f5d486da75bef8bd2dfd1b1de31463e1787d136
Limaneer <shark04486@sharkcoders.pt>
2024-10-11 18:17:06 +0100

    Add files via upload

---
+++ aula01.py
@@ -0,0 +1,14 @@
+print("Exemplo de matemática:")
+
+num1 = 54
+num2 = 18
+print("adiçao")
+print(num1 + num2)
+print("subtração")
+print(num1 - num2)
+print("multiplicação")
+print(num1 * num2)
+print("divisão")
+print(num1 / num2)
+print("resto")
+print(num1 % num2)
---
+++ aula2.py
@@ -0,0 +1,5 @@
+n1=float(input("qual a nota 1?"))
+n2=float(input("qual a nota 2? "))
+media = (n1+n2)/2
+print()
+print(f"A media do aluno é {media}")
